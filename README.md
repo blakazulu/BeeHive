f1fc89beae5fcdef6b1a0cadcf2651f6700c10a9
Liraz Amir <liraz@gimmonix.com>
2019-03-28 11:47:56 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Css-Bee-Hive
+CSS Bee Hive Effect
+
+this is a Bee Hive example with coll hover effect.
---
+++ js/script.js
@@ -0,0 +1,42 @@
+$(function () {
+    $('.tooltip').click(function () {
+        var copyText = $(this).find('p')[0];
+        copyTextToClipboard(copyText.innerHTML);
+
+        $(this).find('span')[0].innerHTML = "Copied: " + copyText.innerHTML;
+    });
+
+    $(".tooltip").mouseout(function () {
+        $(this).find('span')[0].innerHTML = 'Click to copy hex code';
+    });
+
+    function fallbackCopyTextToClipboard(text) {
+        var textArea = document.createElement("textarea");
+        textArea.value = text;
+        document.body.appendChild(textArea);
+        textArea.focus();
+        textArea.select();
+
+        try {
+            var successful = document.execCommand('copy');
+            var msg = successful ? 'successful' : 'unsuccessful';
+            console.log('Fallback: Copying text command was ' + msg);
+        } catch (err) {
+            console.error('Fallback: Oops, unable to copy', err);
+        }
+
+        document.body.removeChild(textArea);
+    }
+
+    function copyTextToClipboard(text) {
+        if (!navigator.clipboard) {
+            fallbackCopyTextToClipboard(text);
+            return;
+        }
+        navigator.clipboard.writeText(text).then(function () {
+            console.log('Async: Copying to clipboard was successful!');
+        }, function (err) {
+            console.error('Async: Could not copy text: ', err);
+        });
+    }
+});
